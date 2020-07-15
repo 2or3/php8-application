4918bba02cb5275a4ad111340c2375c33c8a7159
2or3 <t.nagashima.cog@gmail.com>
2020-07-15 10:13:01 +0900

    fix setup sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,8 @@ cd $entry_point
 /usr/local/bin/aws ecs register-task-definition --cli-input-json file://task_definition_pre.json --region ap-northeast-1
 /usr/local/bin/aws ecs create-service --cli-input-json file://service.json --region=ap-northeast-1
 
+cd terraform
+eval "$(direnv export bash)";
+terraform plan
+terraform apply
+
