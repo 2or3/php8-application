b9813f53532bd2eb324222352278c0e17d013ac3
2or3 <t.nagashima.cog@gmail.com>
2020-07-15 10:12:16 +0900

    add setup sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+entry_point=`pwd`
+
+cd terraform
+eval "$(direnv export bash)";
+terraform plan
+terraform apply
+
+cd $entry_point
+
+/usr/local/bin/aws ecs register-task-definition --cli-input-json file://task_definition_pre.json --region ap-northeast-1
+/usr/local/bin/aws ecs create-service --cli-input-json file://service.json --region=ap-northeast-1
+
